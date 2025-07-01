2d88246e7969f2014ff07ab61397d51607176fdd
Jayakrishnan <jayakrishnanmanupdm@gmail.com>
2025-07-01 13:43:49 +0530

    clean some unwanted files

---
+++ Swimmingpool_competition/athlete_registration.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows;
+
+namespace SwimmingCompetitionManager
+{
+    public partial class AthleteRegistrationWindow : Window
+    {
+        public AthleteRegistrationWindow()
+        {
+            InitializeComponent();
+            dpDOB.SelectedDateChanged += DpDOB_SelectedDateChanged;
+        }
+
+        private void DpDOB_SelectedDateChanged(object sender, RoutedEventArgs e)
+        {
+            if (dpDOB.SelectedDate.HasValue)
+            {
+                var dob = dpDOB.SelectedDate.Value;
+                var today = DateTime.Today;
+                int years = today.Year - dob.Year;
+                int months = today.Month - dob.Month;
+
+                if (today.Day < dob.Day)
+                    months--;
+
+                if (months < 0)
+                {
+                    years--;
+                    months += 12;
+                }
+
+                txtAgeYears.Text = years.ToString();
+                txtAgeMonths.Text = months.ToString();
+            }
+        }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            // Add database saving logic here
+            MessageBox.Show("Registration Successful!", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            txtFirstName.Clear();
+            txtMiddleName.Clear();
+            txtLastName.Clear();
+            cmbGender.SelectedIndex = -1;
+            dpDOB.SelectedDate = null;
+            txtAgeYears.Clear();
+            txtAgeMonths.Clear();
+            txtUID.Clear();
+            txtWhatsApp.Clear();
+            txtEmail.Clear();
+            txtClubID.Clear();
+            txtCategoryID.Clear();
+        }
+    }
+}
